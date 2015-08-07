09ba303947b2aec90110f3db23a44caea28a6d21
Gordon Wells <gordon.wells@gmail.com>
2015-08-07 16:28:31 +0200

    "auto_complete_commit_on_tab" affects contents of autocomplete
    When true "msj" only shows "msj_caf" snippet, when false shows "msj",
    which is present elsewhere in the file, as well

--- CompleteMagic.py
+++ CompleteMagic.py
@@ -20,14 +20,14 @@ import threading
 
 class CommitNextFieldCommand(sublime_plugin.TextCommand):
     def run(self,edit):
-        # self.view.run_command("commit_completion", {})
+        self.view.run_command("commit_completion", {})
         self.view.run_command("next_field", {})
         self.view.run_command("auto_complete", {})
 
 
 class CommitFirstFieldCommand(sublime_plugin.TextCommand):
     def run(self,edit):
-        # self.view.run_command("commit_completion", {})
+        self.view.run_command("commit_completion", {})
         self.view.run_command("auto_complete", {})
 
 
